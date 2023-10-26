35209fe1b121ebb7f3b7ee2f3becb87c030b206a
nargis-akhter <nargis31400@gmail.com>
2023-10-26 19:25:51 -0400

    erd assignment

---
+++ week 5/sql day 2/demo.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from os import getenv
+import psycopg2
+
+load_dotenv()
+user = getenv("USER")
+password = getenv("PASSWORD")
+server = getenv("SERVER")
+print(user, password, server)
+
+pg_connection = psycopg2.connect(
+    dbname=user,
+    user=user,
+    password=password,
+    host=server
+)
+
+cur = pg_connection.cursor()
+
+def read_sql_file(fpath:str):
+    '''Open a SQL file, read it, and return it to us in the function'''
+    with open(fpath, 'r') as f:
+        sql_file = f.read()
+    return sql_file
+
+def create_tables(sql_filepath:str):
+    start = read_sql_file(sql_filepath)
+    tables = start.split(';')
+    for table in tables:
+        try:
+            print(table)
+            cur.execute(table)
+            pg_connection.commit()
+        except psycopg2.ProgrammingError as msg:
+            print(f'Command Skipped: {msg}')
+            
+def insert_data(sql_filepath):
+    start = read_sql_file(sql_filepath)
+    commands = start.split(';')
+    for command in commands:
+        try:
+            print(command)
+            cur.execute(command)
+            pg_connection.commit()
+        except psycopg2.ProgrammingError as msg:
+            print(f'Command Skipped: {msg}')
+            
+if __name__ == '__main__':
+    create_tables(r'C:\Users\nargi\Documents\coding temple\week 5\sql day 2\movie theatre create.sql')
+    insert_data(r'C:\Users\nargi\Documents\coding temple\week 5\sql day 2\movie theatre mock insert.sql')
---
+++ week 5/sql day 2/movie theatre mock insert.sql
@@ -0,0 +1,36 @@
+INSERT INTO customer
+VALUES(
+1,
+'Nargis',
+'Akhter',
+'nargis31400@gmail.com',
+'123-123-1231'
+);
+
+--Insert statement for concession data
+INSERT INTO concession
+VALUES(
+1,
+'snack',
+'popcorn',
+1
+);
+
+--Insert statement for tickets data
+INSERT INTO tickets
+VALUES(
+1,
+0000000,
+8,
+67,
+02
+);
+
+--Insert statement for tickets data
+INSERT INTO movie
+VALUES(
+1,
+'The Notebook',
+'Romance',
+'15-Oct-23'
+);
